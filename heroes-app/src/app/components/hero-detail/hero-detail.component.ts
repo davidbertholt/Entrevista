2b06c6675ad2b2e687345e62d15f33e2ab1ae090
David Bertholt <david.bertholt@intellisite.io>
2022-01-19 12:01:58 -0300

    add on Destroy

--- heroes-app/src/app/components/hero-detail/hero-detail.component.ts
+++ heroes-app/src/app/components/hero-detail/hero-detail.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 // Route
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 // Interface
 import { IHero } from 'src/app/interfaces/IHero';
@@ -14,9 +15,10 @@ import { HeroService } from 'src/app/services/hero/hero.service';
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css']
 })
-export class HeroDetailComponent implements OnInit {
+export class HeroDetailComponent implements OnInit, OnDestroy {
   idHero:any;
   heroe!: IHero;
+  clientSubscription!: Subscription;
   constructor(
     private rutaActiva: ActivatedRoute,
     private heroService: HeroService
@@ -26,11 +28,16 @@ export class HeroDetailComponent implements OnInit {
     this.getHero();
   }
 
+  ngOnDestroy() {
+    this.clientSubscription?.unsubscribe();
+  }
+
   getHero = () => {
     this.rutaActiva.params.subscribe(
       (params: Params) => {
         this.idHero = params;
       });
-      this.heroService.getHeroByID(this.idHero.id).subscribe(res => this.heroe = res);
+      const heroe$ = this.heroService.getHeroByID(this.idHero.id); 
+      this.clientSubscription = heroe$.subscribe(res => this.heroe = res);
   }
 }
