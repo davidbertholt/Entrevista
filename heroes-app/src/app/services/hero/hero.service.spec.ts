8fe12244cc96b858c4fed52e53171b410adb2d69
David Bertholt <david.bertholt@intellisite.io>
2022-01-19 13:19:01 -0300

    add test in HeroService

--- heroes-app/src/app/services/hero/hero.service.spec.ts
+++ heroes-app/src/app/services/hero/hero.service.spec.ts
@@ -1,68 +1,110 @@
-import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-
-import { cold } from 'jasmine-marbles';
-
-import { heroesMock } from 'src/app/mocks/heroes.mock';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { HeroService } from './hero.service';
-import { of, Subject } from 'rxjs';
+import { heroesMock } from 'src/app/mocks/heroes.mock';
+import { IHero } from 'src/app/interfaces/IHero';
 
-
-describe('HeroService', () => {
+fdescribe('HeroService', () => {
+  let httpTestingController: HttpTestingController;
   let service: HeroService;
-  let httpClientSpy: jasmine.SpyObj<HttpClient>;
-
-  const heroesSubject = new Subject();
-
+  const urlBase = "http://localhost:3000/heroes/";
   beforeEach(() => {
-    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
 
-    service = new HeroService(httpClientSpy);
-
-    heroesSubject.next(heroesMock);
 
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [
-        { provide: HeroService, useValue: of(heroesMock) },
-        { provide: HttpClient, useValue: httpClientSpy },
-      ]
     });
+
+    httpTestingController = TestBed.inject(HttpTestingController);
+
+    service = TestBed.inject(HeroService);
+
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  fit('getHeroes called correctly', () => {
-    let call = spyOn(service, 'getHeroes');
-    let spy1 = spyOn(heroesSubject, 'subscribe')
+  it('getHeroes called correctly', (done) => {
+    const heroesUrl = `${urlBase}?superhero_like=&_page=1&_limit=5`;
+    const expectedData = heroesMock;
+    service.getHeroes().subscribe(data => {
+      expect(data.url).toEqual(heroesUrl);
+      expect(data.body).toEqual(expectedData);
+      done();
+    });
 
-    service.getHeroes();
-
-    expect(call).toHaveBeenCalled();
-    expect(spy1).toHaveBeenCalled();
+    const testRequest = httpTestingController.expectOne(heroesUrl);
+    expect(testRequest.request.method).toEqual('GET');
+    testRequest.flush(expectedData);
   });
 
-  it('getHeroByID', () => {
-    expect(service).toBeTruthy();
+  it('getHeroByID is called correctly', (done) => {
+    const id = 1;
+    const heroesUrl = `${urlBase}${id}`;
+    const expectedData = heroesMock.heroes[id];
+    service.getHeroByID(id).subscribe(data => {
+      expect(data).toEqual(expectedData);
+      done();
+    });
+
+    const testRequest = httpTestingController.expectOne(heroesUrl);
+    expect(testRequest.request.method).toEqual('GET');
+    testRequest.flush(expectedData);
   });
 
-  it('addHero', () => {
-    expect(service).toBeTruthy();
+  it('addHero is called correctly', (done) => {
+    const newHero: IHero = {
+      id:21,
+      superhero:"Joker",
+      publisher:"DC Comics",
+      alterEgo:"No se conoce",
+      firstAppearance:"La ultima sonrisa",
+      characters:"No conocido"
+    }
+    const heroesUrl = `${urlBase}`;
+    const expectedData = heroesMock.heroes;
+    service.addHero(newHero).subscribe(data => {
+      done();
+    });
+
+    const testRequest = httpTestingController.expectOne(heroesUrl);
+    expect(testRequest.request.method).toEqual('POST');
+    expect(testRequest.request.body).toEqual(newHero);
+    testRequest.flush(expectedData);
   });
 
-  it('setHero', () => {
-    expect(service).toBeTruthy();
+  it('setHero is called correctly', (done) => {
+    const editedHero: IHero = { ...heroesMock.heroes[1], superhero: "CAMBIADO"};
+    const id = heroesMock.heroes[1].id;
+    const heroesUrl = `${urlBase}${id}`;
+    const expectedData = heroesMock.heroes[id];
+    service.setHero(editedHero).subscribe(data => {
+      done();
+    });
+
+    const testRequest = httpTestingController.expectOne(heroesUrl);
+    expect(testRequest.request.method).toEqual('PUT');
+    expect(testRequest.request.body).toEqual(editedHero);
+    testRequest.flush(expectedData);
   });
 
-  it('deleteHero', () => {
-    expect(service).toBeTruthy();
-  });
+  it('deleteHero is called correctly', (done) => {
+    const deletedHero: IHero = heroesMock.heroes[1];
+    const id = heroesMock.heroes[1].id;
+    const heroesUrl = `${urlBase}${id}`;
+    const expectedData = heroesMock.heroes[id];
+    service.deleteHero(deletedHero).subscribe(data => {
+      done();
+    });
 
-  it('setPag', () => {
-    expect(service).toBeTruthy();
+    const testRequest = httpTestingController.expectOne(heroesUrl);
+    expect(testRequest.request.method).toEqual('DELETE');
+    testRequest.flush(expectedData);
   });
 });
